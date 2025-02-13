77ca8a9683824815c470bd4bd6bcdadd2ec257c1
NM Technology <58670832+nmtechnology@users.noreply.github.com>
2025-02-12 21:33:54 -0700

    wayyy better dashboard look still trying to get index items showing

--- resources/js/Components/alert.vue
+++ resources/js/Components/alert.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="rounded-md bg-green-50 p-4 min-h-full">
+    <div class="rounded-md bg-green-50 p-4 min-h-10 w-10/12">
       <div class="flex">
         <div class="shrink-0">
           <CheckCircleIcon class="size-8 text-green-400" aria-hidden="true" />
         </div>
         <div class="ml-3">
-          <p class="text-sm font-medium text-green-800">{{ $page.props.flash.message }}</p>
+          <p class="font-medium text-green-800 text-4xl">{{ $page.props.flash.message }}</p>
         </div>
         <div class="ml-auto pl-3">
           <div class="-mx-1.5 -my-1.5">
